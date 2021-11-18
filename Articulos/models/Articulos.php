81573eea9673c5d34027825cfb2b2518c141c556
Victorfrancol <victor.franco@unah.hn>
2021-11-17 21:28:30 -0600

    Carga de Proyecto Consolidado

---
+++ Articulos/models/Articulos.php
@@ -0,0 +1,71 @@
+<?php
+
+class Articulos extends conectar{
+
+    public function get_articulos(){
+        $conectar=parent::conexion();
+        parent::set_names();
+        $sql="SELECT * FROM ma_articulos WHERE estado=1";
+        $sql=$conectar->prepare($sql);
+        $sql->execute();
+        return $resultado=$sql->fetchall(PDO::FETCH_ASSOC);
+    }
+
+    public function get_uno($ID){
+        $conectar = parent::conexion();
+        parent::set_names();
+        $sql="SELECT * FROM ma_articulos WHERE estado= 1 AND ID = ?";
+        $sql=$conectar->prepare($sql);
+        $sql->bindvalue(1,$ID);
+        $sql->execute();
+        return $resultado=$sql->fetchaLL(PDO::FETCH_ASSOC);
+    }
+    public function insert_articulos($DESCRIPCION,$UNIDAD,$COSTO,$PRECIO,$APLICA_ISV,$PORCENTAJE_ISV,$ESTADO,$ID_SOCIO){
+        $conectar=parent::conexion();
+        parent::set_names();
+        $sql= "insert into ma_articulos(ID,DESCRIPCION,UNIDAD,COSTO,PRECIO,APLICA_ISV,PORCENTAJE_ISV,ESTADO,ID_SOCIO)
+        VALUES (NULL,?,?,?,?,?,?,?,?);";
+        $sql=$conectar->prepare($sql);
+        $sql->bindValue(1,$DESCRIPCION);
+        $sql->bindValue(2,$UNIDAD);
+        $sql->bindValue(3,$COSTO);
+        $sql->bindValue(4,$PRECIO);
+        $sql->bindValue(5,$APLICA_ISV);
+        $sql->bindValue(6,$PORCENTAJE_ISV);
+        $sql->bindValue(7,$ESTADO);
+        $sql->bindValue(8,$ID_SOCIO);
+        $sql->execute();
+        return $resultado=$sql->fetchall(PDO::FETCH_ASSOC);
+
+    }
+
+    public function update_articulos($ID,$DESCRIPCION,$UNIDAD,$COSTO,$PRECIO,$APLICA_ISV,$PORCENTAJE_ISV,$ESTADO,$ID_SOCIO){
+        $conectar=parent::conexion();
+        parent::set_names();
+        $sql="UPDATE ma_articulos SET DESCRIPCION = ?,UNIDAD = ?,COSTO = ?,PRECIO = ?,APLICA_ISV = ?,PORCENTAJE_ISV = ?,ESTADO = ?,ID_SOCIO = ?
+            WHERE estado = 1 AND ID = ?";
+        $sql=$conectar->prepare($sql);
+        $sql->bindValue(1,$ID);
+        $sql->bindValue(2,$DESCRIPCION);
+        $sql->bindValue(3,$UNIDAD);
+        $sql->bindValue(4,$COSTO);
+        $sql->bindValue(5,$PRECIO);
+        $sql->bindValue(6,$APLICA_ISV);
+        $sql->bindValue(7,$PORCENTAJE_ISV);
+        $sql->bindValue(8,$ESTADO);
+        $sql->bindValue(9,$ID_SOCIO);
+        $sql->execute();
+        return $resultado=$sql->fetchall(PDO::FETCH_ASSOC);
+    }
+
+    public function delete_articulos($ID){
+        $conectar=parent::conexion();
+        parent:: set_names();
+        $sql="DELETE FROM ma_articulos WHERE ID = ?";
+        $sql=$conectar->prepare($sql);
+        $sql->bindValue(1,$ID);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+?>
---
+++ config/conexion.php
@@ -0,0 +1,19 @@
+<?php
+class conectar{
+    protected $dbh;
+    
+    protected function conexion(){
+        try{
+            $conectar = $this->dbh = new PDO("mysql:host=34.68.196.220;dbname=g7_20","G7_20","a60CZ6ku");
+            return $conectar; 
+            } catch (Exception $e){
+                print "Error BD!: " . $e->getMessage() . "<br/>";
+                die ();
+            }
+    }
+    public function set_names(){
+        return $this->dbh->query("SET NAMES 'utf8'");
+    }
+}
+
+?>
